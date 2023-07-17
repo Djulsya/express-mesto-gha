f4353fe1f28b9484b1f6569d60451b720d16e6f4
Jules Bondarenko <psneg76@bk.ru>
2023-07-17 20:37:35 +0300

    hlopshlep

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/Unauthorized');
+const OK = require(../errors/OK);
 
 module.exports = (req, res, next) => {
   const { token } = req.headers;
   let payload;
   if (!token) {
-    next(new Unauthorized('Требуется 123 авторизация'));
+    next(new OK('Запрос прошёл успешно'));
   }
   try {
     payload = jwt.verify(token, 'some-secret-key');
