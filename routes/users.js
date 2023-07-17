f97b0aebf60535a848e7e46941422e225512d551
Jules Bondarenko <psneg76@bk.ru>
2023-07-17 03:14:23 +0300

    hlopshlep

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,32 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
+const { celebrate } = require('celebrate');
+const { Joi } = require('joi');
 
 const usersRouter = express.Router();
+const linkValidation = /(https*:\/\/)([\w-]{1,32}\.[\w-]{1,32})[-._~:/?#@!&'*+,;=]*#*/m; // !!!!!!!!!!!!!
 
 const {
-  getUsers, getUserId, createUser, updateUserAbout, updateUserAvatar,
+  getUsers, getUserId, updateUserAbout, updateUserAvatar,
 } = require('../controllers/users');
 
 usersRouter.get('/users', getUsers);
-usersRouter.get('/users/:userId', getUserId);
-usersRouter.post('/users', createUser);
-usersRouter.patch('/users/me', updateUserAbout);
-usersRouter.patch('/users/me/avatar', updateUserAvatar);
+usersRouter.get('/users/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().required().hex().length(24),
+  }),
+}), getUserId);
+// usersRouter.post('/users', createUser);
+usersRouter.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUserAbout);
+usersRouter.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().pattern(linkValidation),
+  }),
+}), updateUserAvatar);
 
 module.exports = usersRouter;
