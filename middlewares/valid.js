28c3992415eb7ca374e55f754044643cd71495bd
Jules Bondarenko <psneg76@bk.ru>
2023-07-17 18:38:58 +0300

    hlopshlep

---
+++ middlewares/valid.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { Joi, celebrate } = require('celebrate');
+
+const validateURL = /(https?:\/\/(www\.)?)[-._~:/?#@!&'()*+,;=]*#*/m;
+
+module.exports.validateCreateUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(validateURL).required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,13 @@ const express = require('express');
 const usersRouter = express.Router();
 
 const {
-  getUsers, getUserId, createUser, updateUserAbout, updateUserAvatar, updateUser,
+  getUsers, getUserId, updateUserAbout, updateUserAvatar, updateUser,
 } = require('../controllers/users');
 
 usersRouter.get('/users', getUsers);
 usersRouter.get('/users/:userId', getUserId);
 usersRouter.get('/users/me', updateUser);
-usersRouter.post('/users', createUser);
+// usersRouter.post('/users', createUser);
 usersRouter.patch('/users/me', updateUserAbout);
 usersRouter.patch('/users/me/avatar', updateUserAvatar);
 
