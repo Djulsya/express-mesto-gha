9a54df3cd773a3f3de6a0e3bbf1f5d1c6f5dda57
Jules Bondarenko <psneg76@bk.ru>
2023-07-17 23:01:01 +0300

    hlopshlep

--- models/user.js
+++ models/user.js
@@ -21,10 +21,10 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (url) => validator.isURL(url),
       message: 'Некорректный URL',
-      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     },
   },
 
