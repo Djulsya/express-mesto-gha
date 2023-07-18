3aa8e40859b60c54234fadd6c89930b25cd76fb7
Jules Bondarenko <psneg76@bk.ru>
2023-07-19 01:25:43 +0300

    2iter

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
-const NotFound = require('./errors/NotFound');
-
-const {
-  login, createUser,
-} = require('./controllers/users');
-
-const auth = require('./middlewares/auth');
-
-const {
-  JoiValidateLogin, JoiValidateCreateUser,
-} = require('./middlewares/valid');
+const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
@@ -28,18 +15,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(cookieParser());
 app.use(express.json());
 
-app.post('/signin', JoiValidateLogin, login);
-app.post('/signup', JoiValidateCreateUser, createUser);
-
-app.use(auth);
-
-app.use('/cards', cardsRouter);
-app.use('/users', usersRouter);
-
-app.all('*', auth, (req, res, next) => next(
-  new NotFound('Запрашиваемый адрес не найден'),
-));
-
 const handleError = ((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
@@ -49,6 +24,8 @@ const handleError = ((err, req, res, next) => {
 
   next();
 });
+
+app.use(routes);
 
 app.use(errors());
 app.use(handleError);
