1dd64b93bfb3c645567da5691f60fd1e085018c0
Jules <115895273+Djulsya@users.noreply.github.com>
2023-07-30 02:16:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use((req, res, next) => {
- req.user = {
-   _id: '5d8b8592978f8bd833ca8133',
- };
- next();
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133',
+  };
+  next();
 });
 
 const handleError = ((err, req, res, next) => {
